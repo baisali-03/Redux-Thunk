241c232c8a5b0fec027bdde7b0696396b5fa7bac
baisali03 <baisali.pradhan@infoviz.co>
2025-01-20 13:48:41 +0530

    Redux Implemented

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
-import logo from "./logo.svg";
 import "./App.css";
-
+import { useDispatch, useSelector } from "react-redux";
+import { decrement, increment, reset,incrementByAmount } from "./features/counter/couterSlice";
+import { useState } from "react";
 function App() {
+  const [amount, setAmount] = useState(0);
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
+
+  function handleIncrement() {
+    dispatch(increment());
+  }
+
+  function handleDecrement() {
+    dispatch(decrement());
+  }
+
+  function handleReset() {
+    dispatch(reset());
+  }
+  function handleIncrementByAmount() {
+    dispatch(incrementByAmount(amount));
+  }
   return (
     <div className="App">
-      <h1>hello</h1>
+      <button onClick={handleIncrement}>+</button>
+      <p>Counter: {count}</p>
+      <button onClick={handleDecrement}>-</button>
+      <br />
+      <button onClick={handleReset}>Reset</button>
+      <br />
+      <input
+        type="Number"
+        value={amount}
+        placeholder="Enter Amount"
+        onChange={(e) => setAmount(e.target.value)}
+      />
+      <br />
+      <button onClick={handleIncrementByAmount}>Increment by Amount</button>
     </div>
   );
 }
